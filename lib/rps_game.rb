db4717dee3d9a72a010f0b3254c130c400b4c070
DanielArF <daniel_af96@hotmail.com>
2015-10-05 11:27:00 +0200

    WIP, done all the tests and they work, moving on to front end

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,15 +1,22 @@
 module RPS_Game
 
-  attr_accessor :result
+  attr_accessor :Scissors
+  attr_accessor :Rock
+  attr_accessor :Paper
+
 
 	def self.bot_move
 		%w(Rock Paper Scissors).sample
 	end
 
-  def self.RPS_Game(player_move)
+  def self.play(player_move)
     case[player_move, bot_move]
-      when['Rock','Scissors']
+      when['Rock','Scissors'],['Scissors','Paper'], ['Paper', 'Rock']
         result = "You won!"
+      when ['Rock', 'Rock'], ['Scissors', 'Scissors'],  ['Paper', 'Paper']
+        result = "Tie!"
+      when ['Scissors','Rock'],['Paper','Scissors'], ['Rock', 'Paper']
+        result = "Bot won!"
       end
     end
   end
--- spec/rps_game_spec.rb
+++ spec/rps_game_spec.rb
@@ -3,9 +3,63 @@ require 'rps_game'
 
 describe RPS_Game do
 
-	it 'Players wins with rock over bot scissors' do
-		allow(RPS_Game).to recevie(:bot_move).and_return('Scissors')
-		result = RPS_Game.play('Rock')
-		expect(result).to eq "You won!"
-	end
+  describe 'Player won!' do
+    it 'Players wins with Rock over Bot Scissors' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Scissors')
+      result = RPS_Game.play('Rock')
+      expect(result).to eq "You won!"
+    end
+
+    it 'Players wins with Paper over Bot Rock' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Rock')
+      result = RPS_Game.play('Paper')
+      expect(result).to eq "You won!"
+    end
+
+    it 'Players wins with Scissors over Bot Paper' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Paper')
+      result = RPS_Game.play('Scissors')
+      expect(result).to eq "You won!"
+    end
+  end
+
+  describe 'Tie!' do
+    it 'Players and Bot plays Scissors' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Scissors')
+      result = RPS_Game.play('Scissors')
+      expect(result).to eq "Tie!"
+    end
+
+    it 'Players and Bot plays Rock' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Rock')
+      result = RPS_Game.play('Rock')
+      expect(result).to eq "Tie!"
+    end
+
+    it 'Players and Bot plays Paper' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Paper')
+      result = RPS_Game.play('Paper')
+      expect(result).to eq "Tie!"
+    end
+  end
+
+  describe 'Bot won!' do
+    it('Bot wins with Rock over Players Scissors') do
+      allow(RPS_Game).to receive(:bot_move).and_return('Rock')
+      result = RPS_Game.play('Scissors')
+      expect(result).to eq "Bot won!"
+    end
+
+    it 'Bot wins with Paper over Players Rock' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Paper')
+      result = RPS_Game.play('Rock')
+      expect(result).to eq "Bot won!"
+    end
+
+    it 'Bot wins with Scissors over Players Paper' do
+      allow(RPS_Game).to receive(:bot_move).and_return('Scissors')
+      result = RPS_Game.play('Paper')
+      expect(result).to eq "Bot won!"
+    end
+  end
 end
